1fbb71ea5a46fec6114c7ee65334ab55195e781a
HarryGuo <42864587+HarryG55@users.noreply.github.com>
2019-10-25 19:09:53 +0800

    douban Encapsulation)

--- pianyuan/douban.py
+++ pianyuan/douban.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
 
 import requests
-# import re
+import time
+import re
 
-def get_html(url):
+
+
+def get_douban_html(url):
 
     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0"}
     req = requests.get(url,headers = headers)
@@ -12,8 +15,9 @@ def get_html(url):
 
 
 
-
 def get_douban_inf(html):
+
+    douban_info = {}
 
     htmlfile = open(
         html,
@@ -24,68 +28,49 @@ def get_douban_inf(html):
 
     soup = BeautifulSoup(htmlpage, "html.parser")
 
-
-    # 评论
+    soup.find_all
 
     comment_info = soup.find_all(class_="comment")
     for child in comment_info:
         com_name_te = child.find_all(name="a", class_="")
         for childc1 in com_name_te:
             if childc1.string == "展开":
-                print()
+                time.sleep(0.1)
             else:
-                print(childc1.string)
+                douban_info["comment_name"] = childc1.string
 
         com_say = child.find_all(name="span", class_="short")
         com_say_re = child.find_all(name="span", class_="hide-item full")
         for childc2 in com_say_re:
             if childc2.string == "":
                 for childc3 in com_say:
-                    print(childc3.string)
+                    douban_info["comment"] = childc3.string
             else:
-                print(childc2.string)
-
-    # 简介
-    """
-    comment_info = soup.find_all(class_="comment")
-    for child in comment_info:
-        com_name_te = child.find_all(name='a',class_="")
-        for child1 in com_name_te:
-            print(child1.string)
-
-        com = child.find_all(class_="short")
-        if(child.find_all(class_="short"))
-
-
-        for child2 in com:
-            print(child2.string)
-
-        print("")
-
-    """
-
-
-"""
+                    douban_info["comment"] = childc2.string
 
     #获取电影简介（以string形式输出）
+    #Get the movie introduction
     intro_te = soup(name='span',attrs={"property":"v:summary"})
     for child in intro_te:
-        print(child.string)
+        douban_info["introduction"] = child.string
 
     #获取主演名称
+    #Get the actor
     star_te = soup(rel="v:starring")
     for child in star_te:
-        print(child.string)
+        douban_info["actor"] = child.string
 
     #获取类型名称
+    #Get the Type
     cate_te = soup(property="v:genre")
     for child in cate_te:
-        print(child.string)
+        douban_info["type"] = child.string
 
     #获取上映日期
+    #Get the date
     date_te = soup(property="v:initialReleaseDate")
     for child in date_te:
-        print(child.string)
+        douban_info["date"] = child.string
 
 
 
@@ -98,28 +83,20 @@ def get_douban_inf(html):
     compil_otna = r'又名:<.*?> (.+)<.*>'
 
 
-    dire = re.findall(compil_dire,str(mov_info))
-    writ = re.findall(compil_writ,str(mov_info))
-    loca = re.findall(compil_loca,str(mov_info))
-    lang = re.findall(compil_lang,str(mov_info))
-    runt = re.findall(compil_runt,str(mov_info))
-    otna = re.findall(compil_otna,str(mov_info))
+    dire = re.findall(compil_dire,str(htmlpage))
+    writ = re.findall(compil_writ,str(htmlpage))
+    loca = re.findall(compil_loca,str(htmlpage))
+    lang = re.findall(compil_lang,str(htmlpage))
+    runt = re.findall(compil_runt,str(htmlpage))
+    otna = re.findall(compil_otna,str(htmlpage))
 
 
-    print(dire)
-    print(writ)
-    print(loca)
-    print(lang)
-    print(date)
-    print(runt)
-    print(otna)
-
-"""
+    douban_info["director"] = dire
+    douban_info["writer"] = writ
+    douban_info["location"] = loca
+    douban_info["language"] = lang
+    douban_info["runtime"] = runt
+    douban_info["othername"] =otna
 
 
-# 主程序入口，最后封装请直接删除
-if __name__ == "__main__":
-
-
-    html = get_html()
-    get_douban_inf(html)
+    return douban_info
